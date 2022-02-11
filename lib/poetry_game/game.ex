cf656d3035995b6a9efb8309f1cadc01f38bdab4
Gregory McIntyre <greg@gregorymcintyre.com>
2022-02-12 10:48:30 +1100

    Make PoetryGame.Game to manage game state transitions

--- lib/poetry_game/game.ex
+++ lib/poetry_game/game.ex
@@ -1,138 +1,136 @@
 defmodule PoetryGame.Game do
-  alias PoetryGame.Room
+  defstruct(
+    members: %{},
+    seats: [],
+    chat_messages: []
+  )
 
-  @name :poetry_game
-
-  @initial_game_state %{
-    players: []
-  }
-
-  @initial_paper %{
-    word: nil,
-    question: nil,
-    poem: nil
-  }
-
-  @min_players 3
-
-  def start_link(opts \\ []) do
-    opts = Keyword.put_new(opts, :name, @name)
-    state = add_initial_state(Room.initial_state())
-    Agent.start_link(fn -> state end, opts)
+  def init() do
+    %__MODULE__{
+      members: %{},
+      seats: [],
+      chat_messages: []
+    }
   end
 
-  defp add_initial_state(state) do
-    %{state | game: @initial_game_state}
+  def start(game) do
+    players =
+      game.members
+      |> Enum.with_index()
+      |> Enum.sort_by(fn {_, index} -> index end)
+
+    seats =
+      players
+      |> Enum.map(fn {{user_id, _member}, _index} ->
+        %{papers: [initial_paper()]}
+      end)
+
+    members =
+      players
+      |> Enum.map(fn {{user_id, member}, index} ->
+        {user_id, Map.put(member, :seat_index, index)}
+      end)
+      |> Enum.into(%{})
+
+    %{game | seats: seats, members: members}
   end
 
-  def start_game(pid) do
-    Agent.get_and_update(pid, fn state ->
-      cond do
-        map_size(state.room.members) < @min_players ->
-          {{:error, :too_few_players}, state}
-
-        true ->
-          new_state = new_game_state(state)
-          {{:ok, new_state}, new_state}
-      end
-    end)
+  def add_member(game, %{id: id, name: name, color: color}) do
+    if Map.has_key?(game.members, id) do
+      {:error, :already_added}
+    else
+      members = Map.put(game.members, id, %{id: id, name: name, color: color})
+      %{game | members: members}
+    end
   end
 
-  defp new_game_state(state) do
-    new_players =
-      state.room.members
-      |> Enum.map(&player_from_member/1)
-
-    put_in(state, [:game, :players], new_players)
+  def remove_member(game, user_id) do
+    if Map.has_key?(game.members, user_id) do
+      members = Map.delete(game.members, user_id)
+      %{game | members: members}
+    else
+      {:error, :not_found}
+    end
   end
 
-  # TODO: decide what to do when somebody up and leaves mid-game
-  #
-  # Currently, if one of the players leaves, just end the game completely and
-  # potentially re-start it
-  #
-  def remove_player(pid, user_name) do
-    Agent.get_and_update(pid, fn state ->
-      if is_playing(state, user_name) do
-        new_state = %{state | game: @initial_game_state}
-        {{:ok, new_state}, new_state}
-      else
-        {{:ok, state}, state}
-      end
-    end)
+  def set_word(game, user_id, word) do
+    game
+    |> set_value(user_id, :word, word)
+    |> move_paper_to_next_seat(user_id)
   end
 
-  defp is_playing(state, user_name) do
-    index =
-      state.game.players
-      |> Enum.find_index(fn u -> user_name == u.name end)
-
-    index != nil
+  def set_value(game, user_id, key, value) do
+    index = user_seat_index(game, user_id)
+    seats = put_in(game.seats, [Access.at!(index), :papers, Access.at!(0), key], value)
+    %{game | seats: seats}
   end
 
-  def set_word(pid, user_name, word) do
-    set_value(pid, user_name, :word, word, false)
+  def user_seat_index(game, user_id) do
+    get_in(game.members, [user_id, :seat_index])
   end
 
-  def set_question(pid, user_name, question) do
-    set_value(pid, user_name, :question, question, false)
-  end
+  def move_paper_to_next_seat(game, user_id) do
+    old_index = user_seat_index(game, user_id)
+    num_seats = length(game.seats)
+    insert_index = rem(old_index + 1, num_seats)
 
-  def set_poem(pid, user_name, poem) do
-    set_value(pid, user_name, :poem, poem, true)
-  end
+    # remove paper from current seat paper list
+    {paper, seats} = pop_in(game.seats, [Access.at!(old_index), :papers, Access.at!(0)])
 
-  defp player_from_member({name, _member}) do
-    %{name: name, papers: [@initial_paper]}
-  end
-
-  defp update_paper_in_place(players, player_index, new_paper) do
-    put_in(
-      players,
-      [Access.at(player_index), :papers, Access.at(0)],
-      new_paper
-    )
-  end
-
-  defp update_paper_and_move(players, player_index, new_paper) do
-    {_old_paper, players} =
-      pop_in(
-        players,
-        [Access.at(player_index), :papers, Access.at(0)]
+    # append it to the new seat paper list
+    seats =
+      update_in(
+        seats,
+        [Access.at!(insert_index), :papers],
+        fn papers -> List.insert_at(papers, -1, paper) end
       )
 
-    insert_index = rem(player_index + 1, length(players))
-
-    update_in(
-      players,
-      [Access.at(insert_index), :papers],
-      fn papers -> List.insert_at(papers, -1, new_paper) end
-    )
+    %{game | seats: seats}
   end
 
-  defp set_value(pid, user_name, key, value, is_last_key) do
-    Agent.get_and_update(pid, fn state ->
-      old_index =
-        state.game.players
-        |> Enum.find_index(fn u ->
-          user_name == u.name
-        end)
-
-      old_player = state.game.players |> Enum.at(old_index)
-
-      [old_paper | _] = old_player.papers
-
-      new_paper = %{old_paper | key => value}
-
-      new_players =
-        if is_last_key do
-          update_paper_in_place(state.game.players, old_index, new_paper)
-        else
-          update_paper_and_move(state.game.players, old_index, new_paper)
-        end
-
-      new_state = put_in(state, [:game, :players], new_players)
-      {{:ok, new_state}, new_state}
-    end)
+  def player_list(game) do
+    game.seats
+    |> Enum.filter(fn %{user_id: user_id} -> Map.has_key?(game.members, user_id) end)
+    |> Enum.map(fn %{user_id: user_id} -> Map.get(game.members, user_id) end)
   end
+
+  def paper_list(game) do
+    game.seats
+    |> Enum.flat_map(fn %{papers: papers} -> papers end)
+  end
+
+  defp initial_paper() do
+    %{
+      id: Ecto.UUID.generate(),
+      word: nil,
+      question: nil,
+      poem: nil
+    }
+  end
+
+  # defp set_value(pid, user_name, key, value, is_last_key) do
+  #   Agent.get_and_update(pid, fn state ->
+  #     old_index =
+  #       state.game.players
+  #       |> Enum.find_index(fn u ->
+  #         user_name == u.name
+  #       end)
+
+  #     old_player = state.game.players |> Enum.at(old_index)
+
+  #     [old_paper | _] = old_player.papers
+
+  #     new_paper = %{old_paper | key => value}
+
+  #     new_players =
+  #       if is_last_key do
+  #         update_paper_in_place(state.game.players, old_index, new_paper)
+  #       else
+  #         update_paper_and_move(state.game.players, old_index, new_paper)
+  #       end
+
+  #     new_state = put_in(state, [:game, :players], new_players)
+  #     {{:ok, new_state}, new_state}
+  #   end)
+  # end
 end

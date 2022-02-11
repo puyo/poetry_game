18109733ff1be1ba91af052340d1ac16cf53b6f2
Gregory McIntyre <greg@gregorymcintyre.com>
2022-02-11 21:44:45 +1100

    I need ecto for uuids

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule PoetryGame.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:ecto_sql, "~> 3.6"},
       {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
       {:floki, ">= 0.30.0", only: :test},
       {:gettext, "~> 0.18"},

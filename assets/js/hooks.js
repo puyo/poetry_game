19359c2002149e2729bfbdb631fb437ad8cca2cc
Gregory McIntyre <greg@gregorymcintyre.com>
2022-02-11 14:24:19 +1100

    Get chat working, scroll to bottom on change

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -1,34 +1,48 @@
-Hooks = {}
+Hooks = {};
 
 Hooks.GameSize = {
   mounted() {
-    this.width = 0
-    this.height = 0
+    this.width = 0;
+    this.height = 0;
 
     this.resize = (e) => {
-      const newWidth = this.el.clientWidth
-      const newHeight = this.el.clientHeight
+      const newWidth = this.el.clientWidth;
+      const newHeight = this.el.clientHeight;
 
-      if ((newWidth > 0 && newHeight > 0) &&
-        (newWidth !== this.width || newHeight !== this.height)) {
+      if (
+        newWidth > 0 &&
+        newHeight > 0 &&
+        (newWidth !== this.width || newHeight !== this.height)
+      ) {
+        this.width = newWidth;
+        this.height = newHeight;
 
-        this.width = newWidth
-        this.height = newHeight
-
-        this.pushEvent("resize", {width: newWidth, height: newHeight})
+        this.pushEvent("resize", { width: newWidth, height: newHeight });
       }
-    }
+    };
 
-    this.resizeObserver = new ResizeObserver((entries) => {
-      this.resize()
-    })
-
-    this.resizeObserver.observe(this.el)
+    this.resizeObserver = new ResizeObserver(this.resize);
+    this.resizeObserver.observe(this.el);
   },
   unmounted() {
-    this.resizeObserver.unobserve(this.el)
-  }
-}
+    this.resizeObserver.unobserve(this.el);
+  },
+};
 
-export default Hooks
+Hooks.ScrollToBottomOnInput = {
+  mounted() {
+    const scrollToBottom = (_e) => {
+      this.el.scrollTop = this.el.scrollHeight - this.el.clientHeight;
+    };
+    const config = { childList: true };
 
+    this.resizeObserver = new MutationObserver(scrollToBottom);
+    this.resizeObserver.observe(this.el, config);
+    scrollToBottom();
+  },
+  unmounted() {
+    this.resizeObserver.unobserve(this.el);
+  },
+};
+
+export default Hooks;
--- lib/poetry_game_web/live/chat_live.ex
+++ lib/poetry_game_web/live/chat_live.ex
@@ -1,9 +1,6 @@
 defmodule PoetryGame.ChatLive do
   use Phoenix.LiveView,
-    container:
-      {:div,
-       class:
-         "chat-live h-full text-black bg-white max-w-sm border-l-2 border-slate-300 flex flex-col"},
+    container: {:div, class: "h-full"},
     layout: {PoetryGameWeb.LayoutView, "live.html"}
 
   use Phoenix.HTML
@@ -12,30 +9,38 @@ defmodule PoetryGame.ChatLive do
 
   def render(assigns) do
     ~H"""
-    <div class="hidden"><%= @rerender %></div>
-    <div class="user-list shrink p-1 bg-slate-100 border-b-2 border-slate-300 border-solid">
-      <%= for {id, user} <- Map.to_list(@users) do %>
-        <span style={"color: hsl(#{user.color}, 50%, 50%)"}><%= user.name %></span>
-      <% end %>
-    </div>
-    <div class="messages grow text-sm border-b-2 border-slate-300 border-dashed p-1 overflow-y-scroll max-h-[50vh]">
-      <%= for message <- Enum.reverse(@messages) do %>
-        <% name = message["user_name"] %>
-        <% color = message["color"] %>
-        <% content = message["content"] %>
-        <p><span style={"color: hsl(#{color}, 50%, 50%)"}><%= name %></span>&nbsp;: <%= content %></p>
-      <% end %>
-    </div>
-    <div class="entry-form shrink">
-      <form action="#" phx-submit="submit">
-        <%= hidden_input :message, :user_id, value: @user_id  %>
-        <%= hidden_input :message, :user_name, value: @user_name  %>
-        <%= hidden_input :message, :color, value: @user_color  %>
-        <div class="flex items-center justify-center rounded-md">
-          <%= text_input :message, :content, value: @message, class: "inline-flex grow p-2 focus:border-none outline-none border-none" %>
-          <%= submit "Send", class: "inline-flex shrink p-2 hover:bg-slate-300 rounded-md" %>
+    <div class="h-full flex flex-col text-black bg-white max-w-sm border-l-2 border-slate-300">
+      <div class="hidden"><%= @rerender %></div>
+      <div class="user-list shrink p-1 bg-slate-100 border-b-2 border-slate-200 border-solid">
+        <%= for {id, user} <- Map.to_list(@users) do %>
+          <span style={"color: hsl(#{user.color}, 50%, 50%)"}><%= user.name %></span>
+        <% end %>
+      </div>
+      <div class="messages grow" style="position: relative;">
+        <div id={"scroll-to-bottom-#{@topic}"}
+            class="overflow-y-scroll p-1 text-sm border-b-2 border-slate-300 border-dashed"
+            style="position: absolute; top: 0; left: 0; right: 0; bottom: 0;"
+            phx-hook="ScrollToBottomOnInput">
+          <%= for message <- Enum.reverse(@messages) do %>
+            <% name = message["user_name"] %>
+            <% color = message["color"] %>
+            <% content = message["content"] %>
+            <p><span style={"color: hsl(#{color}, 50%, 50%)"}><%= name %></span>&nbsp;: <%= content %></p>
+          <% end %>
         </div>
-      </form>
+      </div>
+      <div class="entry-form shrink">
+        <form action="#" phx-submit="submit">
+          <%= hidden_input :message, :user_id, value: @user_id  %>
+          <%= hidden_input :message, :user_name, value: @user_name  %>
+          <%= hidden_input :message, :color, value: @user_color  %>
+          <div class="flex items-center justify-center">
+            <%= text_input :message, :content, value: @message,
+              class: "inline-flex grow p-2 focus:border-none outline-none border-none" %>
+            <%= submit "Send", class: "inline-flex shrink p-2 hover:bg-slate-300" %>
+          </div>
+        </form>
+      </div>
     </div>
     """
   end
@@ -61,20 +66,7 @@ defmodule PoetryGame.ChatLive do
         user_name: user_name,
         user_color: user_color,
         topic: topic,
-        messages: [
-          %{
-            "color" => 30,
-            "content" => String.duplicate("second chat message", 200),
-            "user_id" => "123",
-            "user_name" => "demo"
-          },
-          %{
-            "color" => user_color,
-            "content" => "first chat message",
-            "user_id" => user_id,
-            "user_name" => user_name
-          }
-        ],
+        messages: [],
         message: "",
         rerender: false,
         users: %{}
@@ -99,7 +91,7 @@ defmodule PoetryGame.ChatLive do
         %{event: "presence_diff", payload: payload},
         %{assigns: %{topic: topic}} = socket
       ) do
-    IO.inspect(presence_diff: 1, payload: payload, users: users(topic))
+    # IO.inspect(presence_diff: 1, payload: payload, users: users(topic))
 
     {
       :noreply,
@@ -114,7 +106,7 @@ defmodule PoetryGame.ChatLive do
         %{event: "chat_message", message: message},
         %{assigns: %{topic: topic}} = socket
       ) do
-    IO.inspect(message_received: message, recipient: self())
+    # IO.inspect(message_received: message, recipient: self())
 
     {
       :noreply,

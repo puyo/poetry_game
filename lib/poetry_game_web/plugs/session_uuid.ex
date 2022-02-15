2006362ce58a0b329f8127826eee829ece11858c
Gregory McIntyre <greg@gregorymcintyre.com>
2022-02-16 09:36:41 +1100

    Tweak randomly assigned user colour range

--- lib/poetry_game_web/plugs/session_uuid.ex
+++ lib/poetry_game_web/plugs/session_uuid.ex
@@ -4,7 +4,9 @@ defmodule PoetryGameWeb.Plugs.SessionUuid do
   def call(conn, _opts) do
     user_id = Plug.Conn.get_session(conn, :user_id) || Ecto.UUID.generate()
     user_name = Plug.Conn.get_session(conn, :user_name) || "anon"
-    user_color = Plug.Conn.get_session(conn, :user_color) || Enum.random(0..255)
+
+    # random range is inclusive
+    user_color = Plug.Conn.get_session(conn, :user_color) || Enum.random(0..359)
 
     conn
     |> Plug.Conn.put_session(:user_id, user_id)

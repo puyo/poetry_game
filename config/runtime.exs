515de7c8d671ddcb5ac90dac3f80efd239813f24
Gregory McIntyre <greg@gregorymcintyre.com>
2022-02-16 19:54:37 +1100

    Do not redirect to example.com

--- config/runtime.exs
+++ config/runtime.exs
@@ -25,7 +25,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || System.get_env("HOST") || "poetry-game.herokuapp.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :poetry_game, PoetryGameWeb.Endpoint,

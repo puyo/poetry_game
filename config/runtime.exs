256968afc32f5f4d9c2cbe13cf2e9973957573b9
Gregory McIntyre <greg@gregorymcintyre.com>
2022-02-16 21:04:47 +1100

    Try fixing the game url

--- config/runtime.exs
+++ config/runtime.exs
@@ -29,7 +29,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :poetry_game, PoetryGameWeb.Endpoint,
-    url: [host: host, port: 443],
+    url: [scheme: "https", host: host, port: 443],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.

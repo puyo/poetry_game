4ecda0f2b2d9e885f997b2faa162f3c1deab94ea
Gregory McIntyre <greg@gregorymcintyre.com>
2022-02-17 16:19:20 +1100

    De-tailwind header

--- lib/poetry_game_web/live/header_live.ex
+++ lib/poetry_game_web/live/header_live.ex
@@ -11,22 +11,21 @@ defmodule PoetryGame.Live.HeaderLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="bg-white border-b-4 border-black border-solid">
-      <div class="container mx-auto">
-        <div class="flex justify-between items-center">
-          <a href="/" class="logo justify-start px-1 py-2">
-            <img src={Routes.static_path(@socket, "/images/poetry-game.svg")} alt="Poetry Game"/>
-          </a>
-          <%= if @user do %>
-            <a href="#" class="user justify-end p-2 text-xl hover:bg-amber-300 rounded-md" phx-click="show_form">
+    <div class="container">
+      <div class="flex justify-between items-center">
+        <div class="justify-start">
+          <img class="logo" src={Routes.static_path(@socket, "/images/poetry-game.svg")} alt="Poetry Game"/>
+        </div>
+        <%= if @user do %>
+          <div class="justify-end">
+            <button class="btn-show-user-form" phx-click="show_form">
               <% color = @user.color %>
               <% name = @user.name %>
-              <span class="font-semibold text-xl" style={user_hsl(color)}><%= name %></span>
-              <img style="vertical-align: baseline; height: 1em"
-                src={Routes.static_path(@socket, "/images/quill.svg")} alt="Rename" class="inline"/>
-            </a>
-          <% end %>
-        </div>
+              <span class="user-name" style={user_hsl(color)}><%= name %></span>
+              <img class="icon edit-icon" src={Routes.static_path(@socket, "/images/quill.svg")} alt="Rename"/>
+            </button>
+          </div>
+        <% end %>
       </div>
     </div>
     """
